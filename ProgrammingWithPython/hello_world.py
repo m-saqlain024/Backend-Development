213c27dc5820bed5c294fcb73ccfa86786f61815
Being-Murshid <murshid71704@gmail.com>
2024-02-21 05:18:46 -0800

    start lerning backend development

---
+++ ProgrammingWithPython/hello_world.py
@@ -0,0 +1,86 @@
+# print("hello world")
+
+# name = "saqlain"
+# if (name=="saqlain"):
+# print("saqlain")           #thsi is single line
+      #thisdfkahsdjkl
+    #this is mutipple line commment
+
+# a,b,c== 1,2,3
+# print(a)
+# pring(b)
+# print(c)
+
+
+d = 10
+# print(d)
+
+del d
+# print(d)
+
+# string
+name = 'saqo'
+# print(type(name))
+# tuple
+t1 = (1,"hello", 4.5,"A")
+# print(t1[3])
+
+# dictionary
+dic = {'a':12,'b':"saqi"}
+# print(dic['b'])
+# print(type(dic))
+
+str1='hello'
+# print(str1)
+
+
+str2 = ' this is a for find lenght in python '
+
+# print(str2)
+
+
+str3 = 'this is first line '\
+  'this is a second line '
+
+# print(str3)
+
+
+str4 = 'hello '
+str5 = 'ther2! '
+# this is concantation example
+# print(str4 + " " + str5)
+
+
+
+name = " saqlain "
+print(len(name))
+
+
+
+
+
+# print("where do you live ")
+# location = input()
+# print("so i live " + location)
+
+
+
+num1 = str(12)
+
+# num2 = int(" ")
+
+# print(type(num2))
+print(type(num1))
+
+
+num3 = 12
+float(num3)
+
+# print(num3)
+
+
+
+# num4 = int('12.5')
+# print(num4)
+
+print("hello",'you', sep='...', end="lo")
---
+++ ProgrammingWithPython/input-output.py
@@ -0,0 +1,11 @@
+# num = input('Please enter a number:')
+# print(num)
+
+
+num1 = False
+num2 = True
+
+if not(num1) or not(num2):
+    print("hello")
+
+
