efc4b69e4bf9e94d12c1892b75e0b8e112b1163c
Being-Murshid <murshid71704@gmail.com>
2024-03-21 01:59:23 -0700

    function for result

--- ProgrammingWithPython/function.py
+++ ProgrammingWithPython/function.py
@@ -5,3 +5,12 @@ def calculate_tax(bill, tax_rate):
 
 print("total Calculate tax :",calculate_tax(1200, 12))
 print("total Calculate tax :",calculate_tax(13300, 20))
+
+
+
+def result(number):
+    if number > 50:
+        print("pass")
+    else :
+        print("faild")
+
