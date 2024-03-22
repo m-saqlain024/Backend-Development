cdd16e5f2056116712c61e071de76fd7eb5db8ea
Being-Murshid <murshid71704@gmail.com>
2024-03-22 03:45:41 -0700

    start learning data structure

---
+++ ProgrammingWithPython/set.py
@@ -0,0 +1,29 @@
+set_a = { 0,1,2,3,4,5,6}
+set_a.add(9)
+set_a.remove(2)
+set_a.discard(1)
+
+# print(set_a)
+
+set_b = {1,2,3,4,5,6,8,9,10}
+
+# to find union of the two set
+
+# print(set_a | set_b)
+# print(set_a.union(set_b))
+
+# to find interstection of two set
+
+# print(set_a.intersection(set_b))
+# print(set_a & set_b)
+
+
+# find differenve of two set , mean set A have element but set B does not have
+
+# print(set_a.difference(set_b))
+# print(set_a -set_b)
+
+# the element which are not commaon in both sets
+
+# print(set_a.symmetric_difference(set_b))
+# print(set_a ^ set_b)
