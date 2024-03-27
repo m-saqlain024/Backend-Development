f028766bc9b60021bb383d7e813fb60bde12d6d8
Being-Murshid <murshid71704@gmail.com>
2024-03-27 01:49:37 -0700

    create a file using py file handling

--- ProgrammingWithPython/file_handling.py
+++ ProgrammingWithPython/file_handling.py
@@ -10,4 +10,17 @@ file.close()
 
 with open('test.txt', mode='r') as file:
     datas = file.readline()
-    print(datas)
+    # print(datas)
+
+
+
+with open('test.txt', mode='w') as file:
+    file.writelines(['this is a first line\n', 'this is a second line\n', 'this is a third line\n'])
+   #/n for new line
+
+
+
+
+with open('test.txt', mode='a') as file:
+    file.writelines(['this is a first line\n', 'this is a second line\n', 'this is a third line\n'])
+   #/n for new line
