a2d9c521e88b79351df3d331913f74f92be934cb
Being-Murshid <murshid71704@gmail.com>
2024-03-27 02:24:54 -0700

    learn create file in py

--- ProgrammingWithPython/file_handling.py
+++ ProgrammingWithPython/file_handling.py
@@ -8,8 +8,8 @@ data = file.readline()
 file.close()
 
 
-with open('test.txt', mode='r') as file:
-    datas = file.readline()
+# with open('sample/test.txt', mode='+r') as file:
+    # datas = file.readline()
     # print(datas)
 
 
@@ -19,8 +19,8 @@ with open('test.txt', mode='w') as file:
    #/n for new line
 
 
-
-
-with open('test.txt', mode='a') as file:
-    file.writelines(['this is a first line\n', 'this is a second line\n', 'this is a third line\n'])
-   #/n for new line
+try:
+    with open('test.txt', mode='a') as file:
+       file.writelines(['this is a first line\n', 'this is a second line\n', 'this is a third line\n'])
+except FileNotFoundError as e:
+    print('Error' ,e)
