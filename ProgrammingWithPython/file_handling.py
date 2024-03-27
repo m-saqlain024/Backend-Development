2241509974a2369e47e92fcb6c981e453a52016a
Being-Murshid <murshid71704@gmail.com>
2024-03-27 02:33:39 -0700

    learn about file reading in py

--- ProgrammingWithPython/file_handling.py
+++ ProgrammingWithPython/file_handling.py
@@ -14,13 +14,20 @@ file.close()
 
 
 
-with open('test.txt', mode='w') as file:
+with open('test.txt', mode='a') as file:
     file.writelines(['this is a first line\n', 'this is a second line\n', 'this is a third line\n'])
    #/n for new line
 
 
-try:
-    with open('test.txt', mode='a') as file:
-       file.writelines(['this is a first line\n', 'this is a second line\n', 'this is a third line\n'])
-except FileNotFoundError as e:
-    print('Error' ,e)
+# try:
+#     with open('test.txt', mode='a') as file:
+#        file.writelines(['this is a first line\n', 'this is a second line\n', 'this is a third line\n'])
+# except FileNotFoundError as e:
+#     print('Error' ,e)
+
+
+
+with open('test.txt' , 'r') as file:
+    # print(file.read(10))
+    # print(file.readline(10))
+    print(file.readlines())
