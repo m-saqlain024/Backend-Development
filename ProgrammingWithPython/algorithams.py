dbc46756593e87d3e78871f1f28754b7ad974de9
Being-Murshid <murshid71704@gmail.com>
2024-03-28 00:30:49 -0700

    first algorithams for checking palinderms

--- ProgrammingWithPython/algorithams.py
+++ ProgrammingWithPython/algorithams.py
@@ -1,10 +1,23 @@
 # algorithams for plaindrame
 
-str = 'reacecae'
+# str = 'reacecae'
 
-start = 0
-last = len(str)-1
+# start = 0
+# last = len(str)-1
 
 
-print(str[start])
-print(str[last])
+# print(str[start])
+# print(str[last])
+
+
+
+def isPalinders(str):
+    start = 0
+    last = len(str) -1
+    for x in str:
+        if str[start] != str[last]:
+            return False
+    return True
+
+
+print(isPalinders('racecar'))
