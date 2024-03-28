9508f7198f9c4cc85afc62b2b8f3b5fdf307458f
Being-Murshid <murshid71704@gmail.com>
2024-03-28 00:58:43 -0700

    custom and build in functions

---
+++ ProgrammingWithPython/functionl-programming.py
@@ -0,0 +1,19 @@
+# custion function
+
+
+developer = ['imran ', 'saqlain', 'rashid', 'irfan']
+
+def reverse(str):
+    return str[::-1]
+
+
+all_developer = map(reverse, developer )
+
+# for x in all_developer:
+    # print(x)
+
+
+# build in  function 
+
+
+print(sorted(developer))
