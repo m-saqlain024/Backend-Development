b3d284690ff9ca9cd6c1c40d0c11048ce79ee9c8
Being-Murshid <murshid71704@gmail.com>
2024-03-28 01:14:46 -0700

    create a non pure fun to pure

--- ProgrammingWithPython/functionl-programming.py
+++ ProgrammingWithPython/functionl-programming.py
@@ -16,4 +16,26 @@ all_developer = map(reverse, developer )
 # build in  function 
 
 
-print(sorted(developer))
+# print(sorted(developer))
+
+
+# pure function 
+
+global_list = [1,2,3]
+
+# def add(item):
+#     global_list.append(item)
+
+# add(4)
+# print(global_list)
+# so this is a not pure function because it make changes to global_list
+
+
+def add(gl , item):
+    nl = gl.copy()
+    nl.append(item)
+    return nl
+
+new_list =  add(global_list,4)
+print(global_list)
+print(new_list)
