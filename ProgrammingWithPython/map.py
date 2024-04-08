0a5a360e0857348c94965c9ae1cd6d7172a3ffae
Being-Murshid <murshid71704@gmail.com>
2024-04-08 00:26:53 -0700

    learn about map

---
+++ ProgrammingWithPython/map.py
@@ -0,0 +1,16 @@
+menu = ['assdf' , 'adgkhk','adkfjshk','ajhsdfjf','dkhdjf','hsjkdkjf']
+
+def myfun(cofee):
+    if cofee[0] == 'a':
+        return cofee
+
+
+
+menulsit = map(myfun , menu)
+
+
+print(menulsit)
+
+
+for x in menulsit:
+    print(x)
