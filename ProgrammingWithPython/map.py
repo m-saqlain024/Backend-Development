38ed6a4582fe1c7a5e1919f119b9840e6127ba46
Being-Murshid <murshid71704@gmail.com>
2024-04-15 00:15:06 -0700

    complete an assement

--- ProgrammingWithPython/map.py
+++ ProgrammingWithPython/map.py
@@ -15,5 +15,42 @@ menulsit = map(myfun , menu)
 
 
 filter_menu = filter(myfun, menu)
-for x in filter_menu:
-    print(x)
+# for x in filter_menu:
+    # print(x)
+
+# asdjh = [[96], [69]]
+
+# print(''.join(list(map(str, asdjh))))
+
+def sum(n):
+   if n == 1:
+       return 0
+   return n + sum(n-1)
+
+a = sum(5)
+# print(a)
+
+
+some = ["aaa", "bbb"]
+
+#1
+# def aa(some):
+#    return
+#2
+# def aa(some, 5):
+#    return
+
+#3
+# def aa():
+#    return
+
+#4
+# def aa():
+#    return "aaa"
+
+numbers = [15, 30, 47, 82, 95]
+def lesser(numbers):
+   return numbers < 50
+
+small = list(filter(lesser, numbers))
+print(small)
