d70c0b00e068cba7e35c2905fac62fe20e24c051
m-saqlain024 <m.saqlain0241@gmail.com>
2024-07-01 01:13:14 -0700

    t01 used different ways to import an module and library

---
+++ ProgrammingWithPython/Imports.py
@@ -0,0 +1,17 @@
+# import math
+# from math import sqrt
+# import math as m
+# from math import factorial as f
+from math import factorial as f, cos as c, sqrt as s, log10 as l
+
+
+# root = math.sqrt(9)
+# root = sqrt(9)
+# cosine = m.cos(0)
+factorial = f(10)
+root = s(16)
+cosine = c(10)
+log = l(10)
+# print(cosine)
+# print(root)
+print(factorial, root , cosine, log)
