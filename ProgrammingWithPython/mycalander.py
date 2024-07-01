3a5de9743f2bb34c0d01411b25c060c0564275ac
m-saqlain024 <m.saqlain0241@gmail.com>
2024-07-01 00:42:50 -0700

    how used module in py

--- ProgrammingWithPython/mycalander.py
+++ ProgrammingWithPython/mycalander.py
@@ -1,3 +1,14 @@
 import sys
 locaation = sys.path
-print(locaation)
+
+
+for i in locaation:
+    print(i)
+
+import calendar
+
+leakdays = calendar.leapdays(2000,2050)
+# print(leakdays)
+
+isleap = calendar.isleap(2000)
+print(isleap)
